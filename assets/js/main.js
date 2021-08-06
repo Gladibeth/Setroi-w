e005be451c53121367f60016bda69ee0483eceda
Gladibeth <gladibethdelosa@gmail.com>
2021-08-06 11:28:32 -0400

    Add slider section cta

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,42 @@ $('.main-video').hover(function toggleControls() {
       this.setAttribute("controls", "controls")
   }
 })
+
+
+$('.cta-slider-js').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  centerMode: true,
+  centerPadding: '40px',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
